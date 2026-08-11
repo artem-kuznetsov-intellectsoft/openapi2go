b5cdff28bc4b66fba4a356d1b768e13406021f4c
artemkuznetsov <artem-kuznetsov-intellectsoft.net>
2026-08-12 01:31:17 +0200

    Auto commit: 2026-08-12 01:31:17

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/artem-kuznetsov-intellectsoft/openapi2go/openapi"
+)
+
+func main() {
+	data, err := os.ReadFile("api/customer.post.json")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "failed to read openapi.json:", err)
+		os.Exit(1)
+	}
+
+	var spec openapi.OpenAPI
+	if err := json.Unmarshal(data, &spec); err != nil {
+		fmt.Fprintln(os.Stderr, "failed to unmarshal openapi.json:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%+v\n", spec.Paths["/customer"].Post)
+}
